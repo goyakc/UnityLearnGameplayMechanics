fb49b03b7489c32e4d2f5877865793808b685945
goyakc <goyakc@roverkids.org>
2026-05-20 14:14:16 -0400

    Version1.2-1

---
+++ GameplayMechanics/GameplayMechanic/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 1.0f;
+    private GameObject focalPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
